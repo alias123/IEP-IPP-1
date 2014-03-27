47a86653896d818d9e007edb561f449bf8c31d2d
Rik Goldman <rikgoldman@gmail.com>
2014-03-27 08:27:32 -0400

    fixed ref

--- jumbotron.php
+++ jumbotron.php
@@ -79,7 +79,7 @@ if(!$services) {
     <title>Jumbotron Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="./jumbotron.css" rel="stylesheet">
